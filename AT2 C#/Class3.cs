d1f33433348bbe9e57f7b1526b48294c469529fe
Fernando Silva Carvalheiro <110870733+FernandoSC0@users.noreply.github.com>
2022-11-06 22:18:39 -0300

    Add files via upload

---
+++ AT2 C#/Class3.cs
@@ -0,0 +1,58 @@
+﻿class Funcionario
+{
+    private string nome;
+    private int matricula;
+    public float salario;
+
+    public Funcionario(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public Funcionario()
+    {
+    }
+
+    public Funcionario(string nome, int matricula, float salario)
+    {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salario = salario;
+    }
+
+    public string getNome()
+    {
+        return this.nome;
+    }
+
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public int getMatricula()
+    {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula)
+    {
+        this.matricula = matricula;
+    }
+
+    public float getSalario()
+    {
+        return this.salario;
+    }
+
+    public void setSalario(float salario)
+    {
+        this.salario = salario;
+    }
+
+    public void imprimir()
+    {
+        Console.WriteLine(this.nome + &quot; &quot; +this.matricula + &quot; &quot; +this.salario);
+    }
+
+}
---
+++ AT2 C#/Program3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Fun[] garage = {
+new Fun(&quot; Fernando & quot;, 11111, 2500.00),
+new Fun(&quot; André & quot;, 22222, 5100.00),
+new Fun(&quot; Paulo & quot;, 33333, 9800.00)
+}
+
+foreach (Fun Funcionario in garage) {
+Console.WriteLine(Funcionario.name);
+Console.WriteLine(Funcionario.matricula);
+Console.WriteLine(Funcionario.salario);
+}
+
+Console.ReadKey();
+}
+}
